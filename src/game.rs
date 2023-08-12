1c4b04865ae3ab5cea471ca8a4e8f4c13a7553fc
umut <umut@sent.com>
2023-08-12 16:35:50 +0300

    game of life bevy

--- src/game.rs
+++ src/game.rs
@@ -1,35 +1,214 @@
-use bevy::prelude::*;
+use std::collections::HashMap;
 
-use crate::GameState;
+use bevy::{
+    input::{gamepad::GamepadAxisChangedEvent, keyboard::KeyboardInput},
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
 
 pub struct GamePlugin;
 
+#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
+pub enum GameState {
+    #[default]
+    Pause,
+    Play,
+}
+
+#[derive(SystemSet, Debug, Eq, PartialEq, Clone, Hash)]
+pub struct Calculate;
+
 #[derive(Resource, Deref, DerefMut)]
-pub struct Score(pub i32);
+pub struct Zoom(f32);
+
+#[derive(Resource)]
+pub struct Cells(HashMap<(i32, i32), bool>);
+
+#[derive(Resource)]
+pub struct CellEntities(HashMap<(i32, i32), Entity>);
 
 #[derive(Component)]
-pub struct Move(f32);
+pub struct Cell {
+    x: i32,
+    y: i32,
+}
 
-#[derive(Component)]
-pub struct Reset(f32, f32);
+#[derive(Event)]
+pub struct NextStepEvent;
+
+#[derive(Resource)]
+pub struct GameTimer(Timer);
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(Score(0))
-            .add_systems(Update, (start_game).run_if(in_state(GameState::MainMenu)))
-            .add_systems(Update, (score_log).run_if(in_state(GameState::Game)));
+        let mut cells = HashMap::new();
+
+        for x in -100..100 {
+            for y in -100..100 {
+                cells.insert((x, y), false);
+            }
+        }
+
+        app.insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
+            .insert_resource(Zoom(0.1))
+            .insert_resource(Cells(cells))
+            .insert_resource(CellEntities(HashMap::new()))
+            .insert_resource(GameTimer(Timer::from_seconds(0.2, TimerMode::Repeating)))
+            .add_event::<NextStepEvent>()
+            .add_state::<GameState>()
+            .add_systems(Startup, init)
+            .add_systems(Update, run_timer.run_if(in_state(GameState::Play)))
+            .add_systems(
+                Update,
+                (pause_play, manuel_step, step, create).in_set(Calculate),
+            )
+            .add_systems(Update, render.after(Calculate));
     }
 }
 
-fn start_game(keys: Res<Input<KeyCode>>, mut next_state: ResMut<NextState<GameState>>) {
+fn init(
+    mut commands: Commands,
+    res: Res<Zoom>,
+    cells: Res<Cells>,
+    mut entities: ResMut<CellEntities>,
+) {
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 100.),
+        projection: OrthographicProjection {
+            scale: res.0,
+            ..default()
+        },
+        ..default()
+    });
+
+    for ((x, y), _) in &cells.0 {
+        let x = *x;
+        let y = *y;
+        let entity = commands
+            .spawn((
+                Cell { x, y },
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::rgb(0., 0., 0.),
+                        custom_size: Some(Vec2::new(1.0, 1.0)),
+                        ..default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(x as f32, y as f32, 0.)),
+                    visibility: Visibility::Hidden,
+                    ..default()
+                },
+            ))
+            .id();
+        entities.0.insert((x, y), entity);
+    }
+}
+
+fn create(
+    mouse: Res<Input<MouseButton>>,
+    window: Query<&Window>,
+    cam: Query<(&Camera, &GlobalTransform)>,
+    mut cells: ResMut<Cells>,
+) {
+    if !mouse.just_pressed(MouseButton::Left) {
+        return;
+    }
+    let window = window.single();
+    let cam = cam.single();
+    let Some(cursor_pos) = window.cursor_position() else {
+        return;
+        };
+    let pos = cam.0.viewport_to_world_2d(cam.1, cursor_pos);
+    let Some(pos) = pos else {
+        return;
+    };
+
+    let x = pos.x.round() as i32;
+    let y = pos.y.round() as i32;
+
+    cells.0.insert((x, y), true);
+}
+
+fn step(mut next_ev: EventReader<NextStepEvent>, mut cells: ResMut<Cells>) {
+    if next_ev.is_empty() {
+        return;
+    }
+    next_ev.clear();
+
+    let mut changes = HashMap::new();
+
+    for ((x, y), alive) in &cells.0 {
+        let live_neigbours = [
+            (x + 1, y + 1),
+            (x + 1, y + 0),
+            (x + 1, y - 1),
+            (x + 0, y - 1),
+            (x + 0, y + 1),
+            (x - 1, y - 1),
+            (x - 1, y + 1),
+            (x - 1, y + 0),
+        ]
+        .iter()
+        .filter(|p| cells.0.get(*p) == Some(&true))
+        .count();
+        if *alive == true {
+            println!("{},{} = {}", x, y, live_neigbours);
+            if live_neigbours < 2 || live_neigbours > 3 {
+                changes.insert((*x, *y), false);
+            }
+        } else {
+            if live_neigbours == 3 {
+                changes.insert((*x, *y), true);
+            }
+        }
+    }
+
+    for (k, v) in changes.iter() {
+        println!("{:?} {:?}", k, v);
+        cells.0.insert(*k, *v);
+    }
+
+    println!("update ....................");
+}
+
+fn pause_play(
+    keys: Res<Input<KeyCode>>,
+    game_state: Res<State<GameState>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
     if keys.just_pressed(KeyCode::Space) {
-        println!("start game");
-        next_state.set(GameState::Game);
+        println!("space");
+        if game_state.get() == &GameState::Play {
+            next_state.set(GameState::Pause);
+        } else {
+            next_state.set(GameState::Play);
+        }
+    }
+}
+fn manuel_step(keys: Res<Input<KeyCode>>, mut next_ev: EventWriter<NextStepEvent>) {
+    if keys.just_pressed(KeyCode::Right) {
+        next_ev.send(NextStepEvent);
     }
 }
 
-fn score_log(score: Res<Score>) {
-    if score.is_changed() {
-        println!("score: {}", score.0);
+fn render(mut q_cells: Query<(&mut Visibility, &Cell)>, cells: Res<Cells>) {
+    for (mut vis, Cell { x, y }) in &mut q_cells {
+        let cell = cells.0.get(&(*x, *y));
+        *vis = if cell == Some(&true) {
+            Visibility::Visible
+        } else {
+            Visibility::Hidden
+        };
+    }
+}
+
+fn run_timer(
+    mut game_timer: ResMut<GameTimer>,
+    time: Res<Time>,
+    mut next_ev: EventWriter<NextStepEvent>,
+) {
+    game_timer.0.tick(time.delta());
+
+    if game_timer.0.just_finished() {
+        next_ev.send(NextStepEvent);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,16 @@ mod game;
 mod ui;
 
 use bevy::{
+    input::common_conditions::input_toggle_active,
     // diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     // input::common_conditions::input_toggle_active,
     prelude::*,
-    render::{
-        camera::{ScalingMode, Viewport},
-        settings::WgpuSettings,
-        RenderPlugin,
-    },
+    render::{settings::WgpuSettings, RenderPlugin},
     text::TextSettings,
-    window::{WindowPlugin, WindowResized, WindowResolution},
+    window::WindowPlugin,
     DefaultPlugins,
 };
-// use bevy_inspector_egui::quick::WorldInspectorPlugin;
-
-#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States, SystemSet)]
-pub enum GameState {
-    #[default]
-    MainMenu,
-    Game,
-}
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     let mut app = App::new();
@@ -47,22 +37,13 @@ fn main() {
             }),
         // LogDiagnosticsPlugin::default(),
         // FrameTimeDiagnosticsPlugin,
-        // WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Escape)),
+        WorldInspectorPlugin::new().run_if(input_toggle_active(false, KeyCode::Escape)),
     ))
     .insert_resource(TextSettings {
         allow_dynamic_font_size: true,
         ..default()
     })
     .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-    .add_state::<GameState>()
-    .add_systems(Startup, init)
     .add_plugins((game::GamePlugin, ui::UIPlugin));
     app.run();
-}
-
-fn init(mut commands: Commands) {
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 100.),
-        ..default()
-    });
 }
