1414e965cf5d0e3fd5e9d3014ef720dce52aec68
umut <umut@sent.com>
2023-08-18 20:19:04 +0300

    wasm

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,2 +1,6 @@
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/conways_game_of_life.wasm
+mv ./out/conways_game_of_life.js ./out/game.js
+wasm-opt -Oz -o ./out/conways_game_of_life_bg.wasm ./out/conways_game_of_life_bg.wasm
+rm ./out/conways_game_of_life.d.ts
+rm ./out/conways_game_of_life_bg.wasm.d.ts
